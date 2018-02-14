81d63477e84e1631dd39a3fa3ec2f08e42a43a72
Christopher Chan <chris.chan@wework.com>
2018-02-14 01:12:56 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,7 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-board = Array.new(9, " ")
+board = Array.new (9, " ")
 
 # code your input_to_index and move method here!
 
